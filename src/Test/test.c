94fce6c264ba0eb556ec7672bc652b53d5437839
阳光是颗柠檬糖 <eternal_f@foxmail.com>
2026-03-10 21:01:10 +0800

    Add files via upload

---
+++ src/Test/test.c
@@ -0,0 +1,97 @@
+#include "test.h"
+#include "bsp_esp8266.h"
+#include "bsp_SysTick.h"
+#include <stdio.h>  
+#include <string.h>  
+#include <stdbool.h>
+#include "bsp_dht11.h"
+#include "bsp_usart.h"
+
+volatile uint8_t ucTcpClosedFlag = 0;
+
+char cStr [ 1500 ] = { 0 };
+
+/**
+  * @brief  ESP8266 （Sta Tcp Client）透传
+  * @param  无
+  * @retval 无
+  */
+void ESP8266_StaTcpClient_UnvarnishTest ( void )
+{
+	uint8_t ucStatus;
+	DHT11_Data_TypeDef DHT11_Data;
+	
+  printf ( "\r\n正在配置 ESP8266 ......\r\n" );
+
+	macESP8266_CH_ENABLE();
+	
+	ESP8266_AT_Test ();
+	
+	ESP8266_Net_Mode_Choose ( STA );
+
+  while ( ! ESP8266_JoinAP ( macUser_ESP8266_ApSsid, macUser_ESP8266_ApPwd ) );	
+	
+	ESP8266_Enable_MultipleId ( DISABLE );
+	
+	while ( !	ESP8266_Link_Server ( enumTCP, macUser_ESP8266_TcpServer_IP, macUser_ESP8266_TcpServer_Port, Single_ID_0 ) );
+	
+	while ( ! ESP8266_UnvarnishSend () );
+	
+	printf ( "\r\n配置 ESP8266 完毕\r\n" );
+	printf ( "\r\n开始透传......\r\n" );
+	
+	while ( 1 )
+	{
+		extern uint8_t flag_ir;
+		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_4) == 0){flag_ir = 1;}
+		if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_4) == 1){flag_ir = 0;}
+		if(DHT11_Data.temp_int>=50){sprintf(cStr,"！！！注意！！当前温度过高！！！\r\n");}
+		if ( DHT11_Read_TempAndHumidity ( & DHT11_Data ) == SUCCESS && flag_ir == 1)       //读取 DHT11 温湿度信息
+		{
+			sprintf ( cStr, "\r\n读取DHT11成功!\r\n\r\n湿度为%d.%d ％RH ，温度为 %d.%d℃ ，当前有人\r\n", 
+								DHT11_Data .humi_int, DHT11_Data .humi_deci, DHT11_Data .temp_int, DHT11_Data.temp_deci );
+		}
+		else if(DHT11_Read_TempAndHumidity ( & DHT11_Data ) == SUCCESS && flag_ir == 0)
+		{
+			sprintf ( cStr, "\r\n读取DHT11成功!\r\n\r\n湿度为%d.%d ％RH ，温度为 %d.%d℃ ，当前无人\r\n", 
+								DHT11_Data .humi_int, DHT11_Data .humi_deci, DHT11_Data .temp_int, DHT11_Data.temp_deci );
+		}
+		else
+			sprintf ( cStr, "Read DHT11 ERROR!\r\n" );
+
+		printf ( "%s", cStr );                                             //打印读取 DHT11 温湿度信息
+
+	
+		ESP8266_SendString ( ENABLE, cStr, 0, Single_ID_0 );               //发送 DHT11 温湿度信息到网络调试助手
+		
+		Delay_ms ( 2000 );
+		
+		if ( ucTcpClosedFlag )                                             //检测是否失去连接
+		{
+			ESP8266_ExitUnvarnishSend ();                                    //退出透传模式
+			
+			do ucStatus = ESP8266_Get_LinkStatus ();                         //获取连接状态
+			while ( ! ucStatus );
+			
+			if ( ucStatus == 4 )                                             //确认失去连接后重连
+			{
+				printf ( "\r\n正在重连热点和服务器 ......\r\n" );
+				
+				while ( ! ESP8266_JoinAP ( macUser_ESP8266_ApSsid, macUser_ESP8266_ApPwd ) );
+				
+				while ( !	ESP8266_Link_Server ( enumTCP, macUser_ESP8266_TcpServer_IP, macUser_ESP8266_TcpServer_Port, Single_ID_0 ) );
+				
+				printf ( "\r\n重连热点和服务器成功\r\n" );
+
+			}
+			
+			while ( ! ESP8266_UnvarnishSend () );		
+			
+		}
+	}
+	
+		
+}
+
+
+
